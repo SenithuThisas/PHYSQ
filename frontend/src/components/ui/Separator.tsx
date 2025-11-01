d8e121bbd9172d1b52facadb5a830241e468c584
SenithuThisas <st20306044@outlook.cardiffmet.ac.uk>
2025-11-02 02:09:18 +0530

    Add Separator component for visual separation with customizable styling

--- frontend/src/components/ui/Separator.tsx
+++ frontend/src/components/ui/Separator.tsx
@@ -1,3 +1,6 @@
-// Separator component placeholder
-export {};
+import React from 'react';
+import { View } from 'react-native';
 
+export default function Separator() {
+  return <View className="h-px bg-white/10" />;
+}
