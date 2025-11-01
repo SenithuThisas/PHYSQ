8361e99f7b821caa39bfec3cc903ed2cd3922500
SenithuThisas <st20306044@outlook.cardiffmet.ac.uk>
2025-11-02 02:08:40 +0530

    Add Icon component to render Ionicons with props support

--- frontend/src/components/ui/Icon.tsx
+++ frontend/src/components/ui/Icon.tsx
@@ -1,3 +1,7 @@
-// Icon component placeholder
-export {};
+import React from 'react';
+import { Ionicons } from '@expo/vector-icons';
 
+type Props = React.ComponentProps<typeof Ionicons>;
+export default function Icon(props: Props) {
+  return <Ionicons {...props} />;
+}
