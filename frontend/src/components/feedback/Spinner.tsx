742ac230c1c94998ba39caddbe72244f739c75e4
SenithuThisas <st20306044@outlook.cardiffmet.ac.uk>
2025-11-02 02:06:45 +0530

    Add Spinner component for loading feedback

--- frontend/src/components/feedback/Spinner.tsx
+++ frontend/src/components/feedback/Spinner.tsx
@@ -1,3 +1,10 @@
-// Spinner component placeholder
-export {};
+import React from 'react';
+import { ActivityIndicator, View } from 'react-native';
 
+export default function Spinner() {
+  return (
+    <View className="items-center justify-center">
+      <ActivityIndicator color="#A259FF" />
+    </View>
+  );
+}
